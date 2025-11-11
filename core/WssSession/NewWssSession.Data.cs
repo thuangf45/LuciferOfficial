b829250678b83b119dd53fe39cc911981c21fd34
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-11-11 23:40:19 +0700

    Add data handling to NewWssSession class
    Introduces a Mapping dictionary for URL routing and a thread-safe Groups collection to manage session groups in the NewWssSession class.

---
+++ core/WssSession/NewWssSession.Data.cs
@@ -0,0 +1,30 @@
+﻿using LuciferCore.NetCoreServer;
+using System.Collections.Concurrent;
+
+namespace Yourspace.Session
+{
+    public partial class NewWssSession
+    {
+        protected Dictionary<string, string> Mapping { get; }
+
+        private readonly ConcurrentDictionary<string, HashSet<NewWssSession>> Groups = new();
+
+        public NewWssSession(WssServer server) : base(server)
+        {
+            // Ẩn URL cho handshake
+            Mapping = new Dictionary<string, string>()
+            {
+                { "/","/index.html" },
+                { "/404", "/404.html" },
+                { "/entertainment", "/entertainment.html" },
+                { "/forum", "/forum.html" },
+                { "/journal", "/journal.html" },
+                { "/search", "/search.html" },
+                { "/shop", "/shop.html" },
+                { "/tool", "/tool.html" },
+                { "/tutorial", "/tutorial.html" }
+            };
+        }
+        
+    }
+}
